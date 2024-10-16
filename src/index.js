1f87c47b78faa9cfaa09baf5535c5ac413ae42c4
NguyenNguyen <nguyenbk92@gmail.com>
2024-10-16 22:53:38 +0700

    error handler and retrieve result

--- src/index.js
+++ src/index.js
@@ -2,14 +2,25 @@ const { PromisePool } = require('@supercharge/promise-pool')
 
 const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time))
 
+const func = async(time) => {
+    await sleep(time)
+    console.log(time)
+    return time
+}
+
 const main = async () => {
     const { results, errors } = await PromisePool
-        .for([1, 10, 2, 3, 4, 5, 6, 7])
+        .for([func(10000), func(10000), func(500), func(200), func(10000),])
         .withConcurrency(2)
-        .process(async num => {
-            await sleep(num * 1000)
-            console.log(num)
+        .handleError(async (error, user) => {
+            console.error(`error $${error} at ${user}`)
         })
+        .process(async(member, index, pool) => {
+            const result = await member
+            return result
+        })
+        console.log(results)
+        console.log(errors)
 }
 
 main()
